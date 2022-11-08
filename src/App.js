549e8d6ceb25040c814bde6d128e28b48462713b
fummon <72984490+fummon@users.noreply.github.com>
2022-11-08 13:31:45 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import Typography from '@mui/material/Typography';
+import NavBar from './components/NavBar';
+import girlCoder from './media/landing-page.svg'; 
+import { Box } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material';
+import React from 'react';
 
 function App() {
+
+  const [mode, setMode] = React.useState('dark');
+
+  const theme = createTheme({
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+            backgroundColor: mode === 'dark' ? '#012030' : '#fff',
+            boxShadow: 'none'
+        }
+      }
+    },
+  },
+});
+
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar/>
       </header>
+      <body className="App-body">
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Typography variant="h3"
+            nowrap
+            component="a"
+            sx={{
+              mr: 1,
+              display: { md: 'flex' },
+              fontFamily: 'Fira Code, monospace',
+              letterSpacing: '.3rem',
+              color: 'white',
+              textDecoration: 'none'}}>Faith Summon</Typography>
+              <Typography
+              variant="h5" 
+              nowrap
+            component="a"
+            sx={{
+              mr: 1,
+              display: { md: 'flex' },
+              fontFamily: 'Fira Code, monospace',
+              letterSpacing: '.3rem',
+              color: '#9AEBA3',
+              textDecoration: 'none'}}>
+                FULL STACK DEVELOPER
+              </Typography>
+              </Box>
+        <img src={girlCoder} style={{width: '30%', display: 'flex', alignContent: 'flex-end'}} ></img>
+      </body>
     </div>
+    </ThemeProvider>
   );
 }
 
